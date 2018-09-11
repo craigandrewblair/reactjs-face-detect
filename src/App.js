7ee42dac547a77a9b3b3cf3109943846499d1d36
Craig Blair <c.andrew.blair@gmail.com>
2018-09-11 22:42:16 +0100

    Implement initial face box implementation

--- src/App.js
+++ src/App.js
@@ -15,9 +15,14 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-        input: '',
-        imageUrl: 'https://samples.clarifai.com/face-det.jpg'
+        input: 'https://samples.clarifai.com/face-det.jpg',
+        imageUrl: 'https://samples.clarifai.com/face-det.jpg',
+        box: {}
     }
+  }
+
+  componentDidMount() {
+    this.onDetectClick();
   }
 
   onInputChange = (event) => {
@@ -27,18 +32,30 @@ class App extends Component {
     });
   }
 
-  onDetectClick = (event) => {
+  getBoxLocation = (location) => {
+    let image = document.getElementById('uploadImage');
+    let boxPos = location.outputs[0].data.regions[0].region_info.bounding_box;
+    let imageHeight = Number(image.height);
+    let imageWidth = Number(image.width);
+    let left_col = boxPos.left_col * imageWidth;
+    let top_row = boxPos.top_row * imageHeight;
+    let right_col = imageWidth - (boxPos.right_col * imageWidth);
+    let bottom_row = imageHeight - (boxPos.bottom_row * imageHeight);
+    this.setState({
+      box: {top_row: top_row, bottom_row: bottom_row, left_col: left_col, right_col: right_col}
+    });
+    console.log(this.state.box);
+  }
+
+  onDetectClick = () => {
     this.setState({
       imageUrl: this.state.input
     });
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.imageUrl).then(
-    function(response) {
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
-    },
-    function(err) {
-      console.log(err);
-    }
-  );
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.imageUrl)
+    .then(response =>
+    this.getBoxLocation(response)
+    .catch(err => console.log(err))
+  )
   }
 
   render() {
@@ -52,7 +69,7 @@ class App extends Component {
       </header>
       <main style={{width:"100vw",display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"space-between"}}>
         <ImageUrlForm onInputChange={this.onInputChange} input={this.state.input} detectClick={this.onDetectClick} />
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box}/>
       </main>
       </div>
     );
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Tilt from 'react-tilt';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({imageUrl}) => {
+const FaceRecognition = ({imageUrl, box}) => {
 
     return (
-        <Tilt style={{width:"40vw", padding:"30px", margin:"30px"}}>
-          <img src={imageUrl} style={{maxWidth:"30vw"}} alt=""/>
-        </Tilt>
+        <div>
+            <Tilt style={{width:"600px", padding:"30px", margin:"30px"}}>
+            <img id="uploadImage" src={imageUrl} style={{maxWidth:"500px"}} alt=""/>
+            <div 
+            className="bounding-box"
+            style={{
+                right: box.right_col,
+                bottom: box.bottom_row,
+                left: box.left_col,
+                top: box.top_row
+            }}>
+            </div>
+            </Tilt>
+        </div>
     )
 
 }
